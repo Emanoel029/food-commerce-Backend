7ce0b248ef3d9260dc0941b9acbbc846a0e79aa9
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-06-04 18:12:08 -0300

    Preparando o Backend

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+
+import express, { Express, Request, Response, response } from "express";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  const { message } = req.body;
+
+  if (!message) return res.status(400).send({ error: "Message is required" });
+
+  res.send({ message });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
